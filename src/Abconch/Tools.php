7460b65a511e9392316e2829d5e073e6526430bc
wangchangjiang <292983934@qq.com>
2018-03-24 16:23:07 +0800

    我的包

---
+++ src/Abconch/Tools.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/3/24 0024
+ * Time: 下午 4:16
+ */
+
+namespace Abconch;
+
+
+class Tools
+{
+    public function __construct()
+    {
+    }
+    public static function Hello(){
+        echo 'Hello world';
+    }
+}
